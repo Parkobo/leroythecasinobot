a37bb352bc81a5f5ee0d20ebaeed83fc7a72a50d
ParkerNZTBrown <90422260+ParkerNZTBrown@users.noreply.github.com>
2022-12-20 21:26:28 -0600

    Adding some code written from other machine, does most of the lottery logic

--- currence_calc.py
+++ currence_calc.py
@@ -1,34 +1,71 @@
-import time
-from decouple import config
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.chrome.service import Service
+import rand
 
-def server_restart():
-    print('Restarting the server via website')
-    chrome_options = Options()
+def make_ticket(ticket_numbers):
+    t_full = []
+    for(i=0;i<5;i++):
+        if (i < 4):
+            t_full[i] = rand(1,10)
+        elif (i == 4):
+            t_full[i] = rand(1,8)
+        else:
+            throw exception
+                print ('Value out of bounds.')
+    return t_full
 
-    chrome_options.add_argument("--window-size=1920,1080")
-    chrome_options.add_argument("--start-maximized")
-    chrome_options.add_argument("--headless")
 
-    server_website = config('server_website', default='')
-    file_driver_path = config('file_path_driver', default='')
-    email_username = config('broccoli_email', default='')
-    broc_password = config('broccoli_password', default='')
+def match_tickets(wt, t):
+    matches = [0, 0]
+    win_four = wt[:3]
+    for(i=0;i<4;i++):
+        if(t[i] in win_four):
+            matches[0] += 1
+    if (t[4] == wt[4]):
+        matches[1] = 1
+    return matches
 
-    s = Service(file_driver_path)
-    web_form = webdriver.Chrome(service=s, options=chrome_options)
-    web_form.get(server_website)
-    web_form.maximize_window()
 
-    time.sleep(1)
-    web_form.find_element(by=By.XPATH, value='/html/body/div/div[1]/div/div/nav/a[2]').click()
-    time.sleep(1)
-    web_form.find_element(by=By.XPATH, value='/html/body/div/div/form/div[1]/div[1]/input').send_keys(email_username)
-    web_form.find_element(by=By.XPATH, value='/html/body/div/div/form/div[1]/div[2]/input').send_keys(broc_password)
-    web_form.find_element(by=By.XPATH, value='/html/body/div/div/form/div[3]/button').click()
-    web_form.find_element(by=By.XPATH, value='/html/body/div[1]/div[2]/main/div/div/nav/div[2]/div[2]/form/input[4]').click()
-    print('Server restarted successfully!')
-    web_form.quit()
+def win_checker(m):
+    payout = 0
+    if(m[1] == 1):
+        if(m[0] == 4):
+            return globals.jackpot
+        elif(m[0] == 3):
+            payout = 25000
+        elif(m[0] == 2):
+            payout = 2500
+        elif(m[0] == 1):
+            payout = 500
+        else:
+            payout = 100
+    else:
+        if(m[0]==4):
+            payout = 50000
+        else:
+            payout = 10000
+
+    if(matches[2] > 0):
+        return(payout * global.pwr_number)
+
+    return payout
+
+
+def run_lottery(player):
+    pwr_number = rand(2,3,4,5,10)
+    win_ticket = []
+    win_ticket = make_ticket(win_ticket)
+    for p in players_db_file():
+        payout = 0
+        pwr_plays = p.pwr_plays
+        for (p.tickets > 0):
+            p.tickets = p.tickets - 1
+            ticket = []
+            ticket = make_ticket(ticket)
+            matches = [0,0,pwr_plays]
+            matches = match_tickets(win_ticket, ticket)
+            if (matches[0] > 0 || matches[1] > 0):
+                payout += win_checker(matches)
+            pwr_plays = pwr_plays - 1
+
+
+
+
